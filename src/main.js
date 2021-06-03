bb29d88ac8743731c498706ddca17915947f41c6
Ian Richardson <ierichar@ucsc.edu>
2021-06-02 21:56:53 -0700

    Ian-massive changes to level transition and spawns

--- src/main.js
+++ src/main.js
@@ -29,9 +29,30 @@ let config = {
 
 let game = new Phaser.Game(config);
 
-//variable
+// Variable -------------------------------------------------------------------
+// Global Level Tracker
+// NOTE: stage divider is +10 between one end and the next start.
+//       Also, you can adjust levels for debug, but we need to decide
+//       on solid, challenging/non-boring level distances.
+let globalLevel = 0;
+// Tutorial             // Current Level Distances:
+let stage0Start = 0;    // 0
+let stage0End = 10;     // 10
+// Stage 1
+let stage1Start = 20;   // 20
+let stage1End = 30;     // 60
+// Stage 2
+let stage2Start = 40;   // 70
+let stage2End = 50;    // 120
+// Stage 3
+let stage3Start = 60;  // 130
+let stage3End = 70;    // 180
+
 const tileSize = 280;
 const globalGravity = 500;
+const elevatorLeft = 190;
+const elevatorRight = 717;
+
 // set UI sizes
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
--- src/scenes/stageComplete.js
+++ src/scenes/stageComplete.js
@@ -22,16 +22,22 @@ class stageComplete extends Phaser.Scene {
         }
 
         this.add.text(game.config.width/2, game.config.height/2 + borderUISize*2 + borderPadding*2, 'Stage Complete', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/2 + borderUISize*3 + borderPadding*3, 'Press (P) to play again!', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2 + borderUISize*3 + borderPadding*3, 'Press (P) to continue!', menuConfig).setOrigin(0.5);
 
         keyP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.P);
     }
 
     update(){
         if (Phaser.Input.Keyboard.JustDown(keyP)) {
+            // // update globalLevel to start new level in play
+            // if (globalLevel >= stage0Start && globalLevel <= stage0End) {
+            //     globalLevel = 20;
+            // } else if (globalLevel >= stage1Start && globalLevel <= stage1End) {
+            //     globalLevel = stage2Start;
+            // } else if (globalLevel >= stage2Start && globalLevel <= stage2End) {
+            //     globalLevel = stage3Start;
+            // }
             this.scene.start('playScene');
         }
-
-
     }
 }

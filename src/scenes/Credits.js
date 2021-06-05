63240c6efafd5424c4f73707df6c1983fbffe7e0
ilda <67297905+ildxa@users.noreply.github.com>
2021-06-05 00:42:42 -1000

    load final assets!!

--- src/scenes/Credits.js
+++ src/scenes/Credits.js
@@ -4,6 +4,7 @@ class Credits extends Phaser.Scene {
     }
 
     preload() {
+        this.load.image('borderScreen', './assets/GameOver/GameOverBorder.png');
         this.load.image('gradient', './assets/SmallBackground.png');
         this.load.image('stars_expanded', './assets/Menu/Stars_Expanded.png');
         this.load.image('creditsLettering', './assets/CreditsLettering.png');
@@ -13,6 +14,8 @@ class Credits extends Phaser.Scene {
     }
 
     create() {
+        this.creditsLettering = this.add.image(120, 100, 'creditsLettering').setOrigin(0, 0);
+        this.border_Screen= this.add.image(120, 100, 'borderScreen').setOrigin(0, 0);
         this.gradient = this.add.tileSprite(0, 0, 960, 640, 'gradient').setOrigin(0, 0);
         this.starfield = this.add.tileSprite(game.config.width/2, 520, 960, 480, 'stars_expanded').setScale(2);
         this.creditsLettering = this.add.image(120, 100, 'creditsLettering').setOrigin(0, 0);
